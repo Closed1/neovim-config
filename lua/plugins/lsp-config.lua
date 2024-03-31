d464e7e9d005e06e137186e670c5693f4a5c2cf6
Lp-sinko <fatiougra@gmail.com>
2024-03-31 14:31:18 +0000

    some fixes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -22,7 +22,6 @@ return{
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-        vim.keymap.set({ "n" }, "<C-l>")
     end
     }
 }
