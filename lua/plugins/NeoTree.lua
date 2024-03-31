4a043115e308ed2f74693d159dc85dc4dc553a86
Lp-sinko <fatiougra@gmail.com>
2024-03-31 13:49:49 +0000

    first commit

---
+++ lua/plugins/NeoTree.lua
@@ -0,0 +1,23 @@
+return
+{"nvim-neo-tree/neo-tree.nvim",
+branch = "v3.x",
+dependencies = {
+"nvim-lua/plenary.nvim",
+"nvim-tree/nvim-web-devicons",
+"MunifTanjim/nui.nvim",},
+    config = function()
+    vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>',{})
+    end
+}
+
+
+
+
+
+
+
+
+
+
+
+
