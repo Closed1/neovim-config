d35ab808d02e3f31f40700f32f309e749f500807
Lp-sinko <fatiougra@gmail.com>
2024-04-01 22:15:39 +0000

    more plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,9 +16,15 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,7 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.diagnostics.codespell,
 			},
