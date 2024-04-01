c925a4609e8e13a9c5166a842066c53661b1d49a
Lp-sinko <fatiougra@gmail.com>
2024-04-01 22:11:49 +0000

    more configs to alpha

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,38 +1,45 @@
 return {
-    "goolord/alpha-nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-        dashboard.section.header.val = {
-            [[                                                   ]],
-            [[                                              ___  ]],
-            [[                                           ,o88888 ]],
-            [[                                        ,o8888888' ]],
-            [[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
-            [[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
-            [[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
-            [[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
-            [[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
-            [[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
-            [[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
-            [[             . ..:.::o:ooooOoCoCCC"o:o             ]],
-            [[             . ..:.::o:o:,cooooCo"oo:o:            ]],
-            [[          `   . . ..:.:cocoooo"'o:o:::'            ]],
-            [[          .`   . ..::ccccoc"'o:o:o:::'             ]],
-            [[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
-            [[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
-            [[     ...:.'.:.::::"'    . . . . .'                 ]],
-            [[    .. . ....:."' `   .  . . ''                    ]],
-            [[  . . . ...."'                                     ]],
-            [[  .. . ."'           N E O V I M                   ]],
-            [[ .                                                 ]],
-            [[                                                   ]],
-        }
-        alpha.setup(dashboard.opts)
-    end,
+		dashboard.section.header.val = {
+			[[                                              ___  ]],
+			[[                                           ,o88888 ]],
+			[[                                        ,o8888888' ]],
+			[[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
+			[[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
+			[[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
+			[[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
+			[[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
+			[[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
+			[[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
+			[[             . ..:.::o:ooooOoCoCCC"o:o             ]],
+			[[             . ..:.::o:o:,cooooCo"oo:o:            ]],
+			[[          `   . . ..:.:cocoooo"'o:o:::'            ]],
+			[[          .`   . ..::ccccoc"'o:o:o:::'             ]],
+			[[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
+			[[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
+			[[     ...:.'.:.::::"'    . . . . .'                 ]],
+			[[    .. . ....:."' `   .  . . ''                    ]],
+			[[  . . . ...."'                                     ]],
+			[[  .. . ."'           N E O V I M                   ]],
+			[[ .                                                 ]],
+			[[                                                   ]],
+		}
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("Ctrl + p", "  Find file"),
+			dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("Leader +fj", "  Find Word"),
+			dashboard.button("s", "   Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+			dashboard.button("q", "  Quit NeoVim", ":qa<cr>"),
+		}
+		alpha.setup(dashboard.opts)
+	end,
 }
