7a55ba71f2a8b25da1b7d86639481e735a046a71
Lp-sinko <fatiougra@gmail.com>
2024-04-05 21:31:26 +0000

    organizing some configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,6 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			vim.keymap.set({ "n", "t" }, "<leader>a", ":Telescope buffers<CR>", {}) --change between buffers
-			vim.keymap.set("n", "<leader>t", ":terminal<CR>", {}) -- quick open terminal
-			vim.keymap.set("t", "<C-x>", "<C-\\><C-n>", { silent = true }) -- exit terminal mode
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fj", builtin.live_grep, {})
