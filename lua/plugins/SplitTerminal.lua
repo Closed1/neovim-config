46704e02a0ef785bc5abf3d1065139ce3af96bb9
Lp-sinko <fatiougra@gmail.com>
2024-04-06 23:58:41 +0000

    a config for hterminal

--- lua/plugins/SplitTerminal.lua
+++ lua/plugins/SplitTerminal.lua
@@ -4,7 +4,7 @@ return {
 		vim.o.splitbelow = true
 		vim.o.splitright = true
 	end,
-	vim.keymap.set("n", "<leader>ht", ":Term<CR>"),
+    vim.keymap.set("n", "<leader>ht", ":12Term<CR>"),
 	vim.keymap.set("n", "<leader>t", ":70VTerm<CR>"),
 	vim.keymap.set("n", "<A-l>", "<C-w>l", {}),
 	vim.keymap.set("t", "<C-x>", "<C-\\><C-n>", {}) -- exit terminal mode or you can just press escape
