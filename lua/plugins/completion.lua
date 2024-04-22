b5d82d0fe42b56b70f9f4c04f564899d5718a305
Lp-sinko <fatiougra@gmail.com>
2024-04-23 00:07:20 +0100

    tab command added

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -32,7 +32,12 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
