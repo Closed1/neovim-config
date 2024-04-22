e749e155e3044bcc2078328bd386a8a58c321bbf
Lp-sinko <fatiougra@gmail.com>
2024-04-22 22:12:14 +0100

    cursorline added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,5 +8,6 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("set cursorline")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
