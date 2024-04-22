50c78c5b6f844426e04a47fedd5772553f88501d
Lp-sinko <fatiougra@gmail.com>
2024-04-23 00:06:34 +0100

    rainbow indent added

--- lua/plugins/IndentLine.lua
+++ lua/plugins/IndentLine.lua
@@ -1,1 +1,17 @@
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = function(_, opts)
+		config =
+			function()
+				require("ibl").setup({
+					scope = {
+						enabled = false,
+					},
+				})
+			end, require("indent-rainbowline").make_opts(opts)
+	end,
+	dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
+}
