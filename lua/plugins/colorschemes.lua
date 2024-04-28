2816220090534eda83995445f15b460fd186effb
Lp-sinko <fatiougra@gmail.com>
2024-04-29 00:45:01 +0100

    colorscheme added

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,17 +1,21 @@
 return {
-    {
-        "ellisonleao/gruvbox.nvim",
-        --setting gurvbox as the default theme
-        config = function()
-            vim.cmd.colorscheme("gruvbox")
-        end,
-    },
+	{
+		"ellisonleao/gruvbox.nvim",
+		--setting gurvbox as the default theme
+		config = function()
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
 
-    {
-        "tanvirtin/monokai.nvim",
-    },
+	{
+		"tanvirtin/monokai.nvim",
+	},
 
-    {
-        "Mofiqul/dracula.nvim",
-    },
+	{
+		"Mofiqul/dracula.nvim",
+	},
+
+	{
+		"projekt0n/github-nvim-theme",
+	},
 }
