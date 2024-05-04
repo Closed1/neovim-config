06bdcda03df56de7dc1c3fb996d2ddaf39370eaa
Lp-sinko <fatiougra@gmail.com>
2024-05-04 17:00:11 +0100

    tweaks

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,6 +3,9 @@ return {
 		"ellisonleao/gruvbox.nvim",
 		--setting gurvbox as the default theme
 		config = function()
+			require("gruvbox").setup({
+				bold = false,
+			})
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
@@ -13,9 +16,5 @@ return {
 
 	{
 		"Mofiqul/dracula.nvim",
-	},
-
-	{
-		"projekt0n/github-nvim-theme",
 	},
 }
