8c9b5505b4ca6731335c88da7e4bf8dd3e874bc3
Lp-sinko <fatiougra@gmail.com>
2024-11-27 10:03:46 +0100

    added some stuff to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
+            ensure_installed ={"c", "lua", "vim", "vimdoc", "rust", "go", "cpp", "javascript", "html", "python","java"},
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
