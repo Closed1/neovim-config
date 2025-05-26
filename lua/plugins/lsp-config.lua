4f661b5443c6f5911e90fea4d06789fa60144af8
Lp-sinko <fatiougra@gmail.com>
2025-05-26 18:23:15 +0100

    removed hints

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,41 +9,43 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls","clangd","pyright","html","unocss"},
-        automatic_installation = true,
+				ensure_installed = { "lua_ls", "clangd", "pyright", "html", "unocss" },
+				automatic_installation = true,
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			vim.diagnostic.config({ signs = true, virtual_text = false, underline = true })
+
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({
-		capabilities = capabilities,
-		})
-		lspconfig.clangd.setup({
-		capabilities = capabilities,
-		})
-		lspconfig.pyright.setup({
-		capabilities = capabilities,
-		})
-		lspconfig.html.setup({
-		capabilities = capabilities,
-		})
-		lspconfig.unocss.setup({
-		capabilities = capabilities,
-		})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.unocss.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 			for type, icon in pairs(signs) do
 				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 			end
+			vim.diagnostic.config({ signs = { text = { [vim.diagnostic.severity.HINT] = "" } } })
 		end,
 	},
 }
